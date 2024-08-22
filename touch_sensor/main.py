880b1ebd9ebb1adb534c13ce64f0d22d3eb7baf7
Win Tun <wintun.hlaing@outlook.com>
2024-08-22 19:54:45 +0630

    touch sensor success

--- touch_sensor/main.py
+++ touch_sensor/main.py
@@ -1,9 +1,9 @@
 from machine import Pin
 import time
 # Initialize the touch sensor pin
-touch_sensor = Pin(5, Pin.IN)
-print("touch value",touch_sensor)
+touch_sensor = Pin(4, Pin.IN)
 while True:
+    print("touch value",touch_sensor.value())
     if touch_sensor.value() == 1:
         print("Touch detected!")
     else:
