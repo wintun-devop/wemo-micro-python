8a82adbcdf63bd840ff6f6970239da0a57826db8
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2024-08-22 10:28:47 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from machine import Pin
 from time import sleep
 
-led = Pin('LED', Pin.OUT)
-print('Blinking LED Example')
+led = Pin(4, Pin.OUT)
 
 while True:
-  led.value(not led.value())
+  led.value(1) # led ON
+  print("LED ON")
+  sleep(0.5)
+  led.value(0) # led OFF
+  print("LED OFF3")
   sleep(0.5)
