ae4a809e9260cf92c3472d3a1464e68daf49c17e
Win Tun <wintun.hlaing@outlook.com>
2024-08-25 12:51:04 +0630

    motion sensor test

--- device_upload/main.py
+++ device_upload/main.py
@@ -1,17 +1,12 @@
-import dht,time
 from machine import Pin
+import time
 
-# Initialize the DHT sensor
-# Use DHT22(Pin(4)) if you're using DHT22
-sensor = dht.DHT11(Pin(4))  
+motion = Pin(4, Pin.IN)  
 while True:
-    try:
-        sensor.measure()
-        temp = sensor.temperature()
-        hum = sensor.humidity()
-        print('Temperature: {}°C'.format(temp))
-        print('Humidity: {}%'.format(hum))
-    except OSError as e:
-        # print("e",e)
-        print('Failed to read sensor.')
-    time.sleep(5)
+    print("all",motion)
+    print("motion:",motion.value())
+    if motion.value() == 1:
+        print("motion detected!")
+    else:
+        print("no motion!")
+    time.sleep(1)
